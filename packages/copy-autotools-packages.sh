ab572b2c9cbe2ea4af981cd36c6c93d1fc1a9585
Tim Stahlhut <stahta01@gmail.com>
2024-12-25 03:57:09 -0500

    packages: Edit copy-autotools-packages.sh

--- packages/copy-autotools-packages.sh
+++ packages/copy-autotools-packages.sh
@@ -1,19 +1,17 @@
-mkdir -p ./autotools_MSYS_32bit_packages
+mkdir -p ./autotools_MSYS_packages
 
-#rm -f ./autotools_MSYS_32bit_packages/*-i686.pkg.tar.zst
+cp --no-clobber ../autotools/autotools-*-any.pkg.tar.zst ./autotools_MSYS_packages/
+cp --no-clobber ../autotools-wrappers/*-any.pkg.tar.zst ./autotools_MSYS_packages/
 
-cp -f ../autotools/autotools-*-any.pkg.tar.zst ./autotools_MSYS_32bit_packages/
-cp -f ../autotools-wrappers/*-any.pkg.tar.zst ./autotools_MSYS_32bit_packages/
+cp --no-clobber ../autoconf2.13/autoconf*-*-any.pkg.tar.zst ./autotools_MSYS_packages/
+cp --no-clobber ../autoconf2.69/autoconf*-*-any.pkg.tar.zst ./autotools_MSYS_packages/
+cp --no-clobber ../autoconf2.71/autoconf*-*-any.pkg.tar.zst ./autotools_MSYS_packages/
+cp --no-clobber ../autoconf2.72/autoconf*-*-any.pkg.tar.zst ./autotools_MSYS_packages/
 
-cp -f ../autoconf2.13/autoconf*-*-any.pkg.tar.zst ./autotools_MSYS_32bit_packages/
-cp -f ../autoconf2.69/autoconf*-*-any.pkg.tar.zst ./autotools_MSYS_32bit_packages/
-cp -f ../autoconf2.71/autoconf*-*-any.pkg.tar.zst ./autotools_MSYS_32bit_packages/
-cp -f ../autoconf2.72/autoconf*-*-any.pkg.tar.zst ./autotools_MSYS_32bit_packages/
-
-cp -f ../automake1.11/automake*-*-any.pkg.tar.zst ./autotools_MSYS_32bit_packages/
-cp -f ../automake1.12/automake*-*-any.pkg.tar.zst ./autotools_MSYS_32bit_packages/
-cp -f ../automake1.13/automake*-*-any.pkg.tar.zst ./autotools_MSYS_32bit_packages/
-cp -f ../automake1.14/automake*-*-any.pkg.tar.zst ./autotools_MSYS_32bit_packages/
-cp -f ../automake1.15/automake*-*-any.pkg.tar.zst ./autotools_MSYS_32bit_packages/
-cp -f ../automake1.16/automake*-*-any.pkg.tar.zst ./autotools_MSYS_32bit_packages/
-cp -f ../automake1.17/automake*-*-any.pkg.tar.zst ./autotools_MSYS_32bit_packages/
+cp --no-clobber ../automake1.11/automake*-*-any.pkg.tar.zst ./autotools_MSYS_packages/
+cp --no-clobber ../automake1.12/automake*-*-any.pkg.tar.zst ./autotools_MSYS_packages/
+cp --no-clobber ../automake1.13/automake*-*-any.pkg.tar.zst ./autotools_MSYS_packages/
+cp --no-clobber ../automake1.14/automake*-*-any.pkg.tar.zst ./autotools_MSYS_packages/
+cp --no-clobber ../automake1.15/automake*-*-any.pkg.tar.zst ./autotools_MSYS_packages/
+cp --no-clobber ../automake1.16/automake*-*-any.pkg.tar.zst ./autotools_MSYS_packages/
+cp --no-clobber ../automake1.17/automake*-*-any.pkg.tar.zst ./autotools_MSYS_packages/
